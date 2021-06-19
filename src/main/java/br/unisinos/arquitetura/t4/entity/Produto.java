122bf5a3e46094a0efa0d70ca3b8daeebc6ad816
Vinicius Santos da Silva <vinicius.unisinos@hotmail.com>
2021-06-19 14:10:12 -0300

    ProdutoController.

---
+++ src/main/java/br/unisinos/arquitetura/t4/entity/Produto.java
@@ -0,0 +1,63 @@
+package br.unisinos.arquitetura.t4.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.management.relation.Role;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+public class Produto extends PanacheEntity {
+
+	private String name;
+
+	private String description;
+
+	@ManyToMany(cascade = { CascadeType.ALL })
+	@JoinTable(name = "Produto_Categoria", joinColumns = { @JoinColumn(name = "produto_id") }, inverseJoinColumns = {
+			@JoinColumn(name = "categoria_id") })
+
+	Set<Categoria> categorias = new HashSet<>();
+
+	public Set<Categoria> getCategorias() {
+		return this.categorias;
+	}
+
+	public void addCategoria(Categoria categoria) {
+		this.categorias.add(categoria);
+	}
+
+	public void setCategorias(Set<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
